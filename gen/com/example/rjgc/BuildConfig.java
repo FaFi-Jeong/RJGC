80adbf80a4bf83b3e1573d3834ab6ae67a7a1cee
郑鑫 <fafismithfates@gmail.com>
2015-05-13 22:46:25 +0800

    开始写app
    第一次提交

---
+++ gen/com/example/rjgc/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.rjgc;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/rjgc/state.java
@@ -0,0 +1,21 @@
+package com.example.rjgc;
+
+import android.content.Context;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+public class state{
+	
+
+	private static final int SCALE_REDUCE_INIT = 0;
+	private static final int SCALING = 1;
+	private static final int SCALE_ADD_INIT = 6;
+
+
+
+}
